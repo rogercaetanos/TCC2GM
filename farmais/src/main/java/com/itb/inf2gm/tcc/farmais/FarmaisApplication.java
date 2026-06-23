244bf93e8a4c99ad433727e42c4fed61fdd046fc
Rogério Caetano <rogercaetanos@gmail.com>
2026-06-23 11:58:13 +0000

    Criação da api back-end com spring boot

---
+++ farmais/src/main/java/com/itb/inf2gm/tcc/farmais/FarmaisApplication.java
@@ -0,0 +1,13 @@
+package com.itb.inf2gm.tcc.farmais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaisApplication.class, args);
+	}
+
+}
